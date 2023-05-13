989caed268b30c2d26c536352c07ffbd0ef6b06b
CodingMarco <codingmarco@gmail.com>
2023-05-13 18:43:49 +0200

    Add cleartext index support with git ls-files

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import os
 import subprocess
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Print the contents of a .git directory.')
+parser.add_argument('path', metavar='path', type=str, nargs='?', default='.git', help='path to the .git directory')
+parser.add_argument('-i', '--ignore', metavar='ignore', type=str, nargs='?', default='hooks', help='comma-separated list of files to ignore')
+parser.add_argument('--cleartext-index', action='store_true', help='print the index file in cleartext using git ls-files --staged instead of printing the raw binary file')
+args = parser.parse_args()
+
 
 def print_tree(dir_path, prefix=''):
     entries = os.scandir(dir_path)
     entries = sorted(entries, key=lambda e: e.is_file())
     entries = sorted(entries, key=lambda e: e.name)
     for i, entry in enumerate(entries):
+        if entry.name in args.ignore.split(','):
+            continue
+
         if i == len(entries) - 1:
             print(prefix + '└──', end='')
             new_prefix = prefix + '   '
@@ -14,11 +26,13 @@ def print_tree(dir_path, prefix=''):
             new_prefix = prefix + '│  '
         if entry.is_file():
             print(entry.name)
-            # Get the output of "git cat-file -p <hash>"
-            # and print it with the new prefix
-            if len(entry.name) == 38:
+            is_git_object = len(entry.name) == 38
+            is_git_index = entry.name == 'index'
+            if is_git_object:
                 hash = entry.path.split('/')[-2] + entry.name
                 lines = subprocess.check_output(['git', 'cat-file', '-p', hash]).decode("UTF-8", "ignore").split('\n')
+            elif is_git_index and args.cleartext_index:
+                lines = subprocess.check_output(['git', 'ls-files', '--stage']).decode("UTF-8", "ignore").split('\n')
             else:
                 with open(entry.path, 'rb') as f:
                     lines = f.read().decode("UTF-8", "ignore").split('\n')
@@ -36,4 +50,5 @@ def print_tree(dir_path, prefix=''):
             print(entry.name + '/')
             print_tree(entry.path, new_prefix)
 
-print_tree('.git')
+
+print_tree(args.path)
