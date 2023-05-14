d9f5567f579e2ac97007dbef779b90019b6203f2
CodingMarco <codingmarco@gmail.com>
2023-05-14 14:04:24 +0200

    Add color support

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import subprocess
 import argparse
 
 
+class colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    ORANGE = '\033[33m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 parser = argparse.ArgumentParser(description='Print the contents of a .git directory.')
 parser.add_argument('path', metavar='path', type=str, nargs='?', default='.git', help='path to the .git directory')
 parser.add_argument('-i', '--ignore', metavar='ignore', type=str, nargs='?', default='hooks', help='comma-separated list of files to ignore')
@@ -10,10 +23,18 @@ parser.add_argument('--cleartext-index', action='store_true', help='print the in
 args = parser.parse_args()
 
 
+def is_likely_blob(lines):
+    return not any(line.startswith('tree ') or line.startswith('parent ') or line.startswith('author ')
+                    or line.startswith('committer ') or line.startswith('encoding ') or line.startswith('gpgsig ')
+                    or line.startswith('mergetag ') or line.startswith('object ') or line.startswith('type ')
+                    or line.startswith('tag ') or line.startswith('tagger ') or " blob " in line or " tree " in line for line in lines)
+
+
 def print_tree(dir_path, prefix=''):
     entries = os.scandir(dir_path)
     entries = sorted(entries, key=lambda e: e.is_file())
     entries = sorted(entries, key=lambda e: e.name)
+
     for i, entry in enumerate(entries):
         if entry.name in args.ignore.split(','):
             continue
@@ -25,12 +46,14 @@ def print_tree(dir_path, prefix=''):
             print(prefix + '├──', end='')
             new_prefix = prefix + '│  '
         if entry.is_file():
-            print(entry.name)
+            print(colors.BOLD + colors.GREEN + entry.name + colors.ENDC)
             is_git_object = len(entry.name) == 38
             is_git_index = entry.name == 'index'
+            likely_blob = False
             if is_git_object:
                 hash = entry.path.split('/')[-2] + entry.name
                 lines = subprocess.check_output(['git', 'cat-file', '-p', hash]).decode("UTF-8", "ignore").split('\n')
+                likely_blob = is_likely_blob(lines)
             elif is_git_index and args.cleartext_index:
                 lines = subprocess.check_output(['git', 'ls-files', '--stage']).decode("UTF-8", "ignore").split('\n')
             else:
@@ -38,14 +61,16 @@ def print_tree(dir_path, prefix=''):
                     lines = f.read().decode("UTF-8", "ignore").split('\n')
 
             # Split lines longer than 80 characters into multiple lines ending with \
-            max_len = 82
+            max_len = 820
             for i, line in enumerate(lines):
                 if len(line) > max_len:
                     lines[i] = line[:max_len] + '\\'
                     lines.insert(i + 1, line[max_len:])
 
+            color = colors.CYAN if likely_blob else colors.ORANGE
+
             for line in lines:
-                print(new_prefix + line.strip())
+                print(new_prefix + color + line.strip() + colors.ENDC)
         else:
             print(entry.name + '/')
             print_tree(entry.path, new_prefix)
